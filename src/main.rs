48c0d637c92bf3baa940d4426d31e592479b956d
muffin <muffin@arch.man>
2025-08-15 12:25:34 +0200

    planed exec loop

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,27 @@ fn main() {
     let board = Board::init_board();
 
     board.print();
+
+    //loop
+        //Si le dernier coup a fait un check 
+            //input + output
+                //Autorise ?
+                //mise en echec ?
+                //obstacle ?
+                //Sortie du check de base ?
+                //Oui 
+                    //Continue
+                //Non 
+                    //Refuser le coup
+        //input : case de depart
+            //si pas de piece COLOR en case de depart on relance
+        //output : case d'arrivee
+        //check la validite du move :
+            //deplacement autorise pour la piece ?
+            //obstacle ?
+            //situation de check avant ? apres ?
+        //Check du pat / mat
+            //si le move met le roi adverse en mat OU si pat : 
+                //break
+        //print
 }
