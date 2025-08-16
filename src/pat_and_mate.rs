0d001f90f34242085fcee366691397dd7cea80d2
muffin <muffin@arch.man>
2025-08-16 12:44:02 +0200

    path and mate.rs

---
+++ src/pat_and_mate.rs
@@ -0,0 +1,24 @@
+
+///if the next player to play has no move possible
+fn is_a_pat() {
+
+}
+
+///Check if on any adjacent case the king could avoid threat
+fn can_king_survive() {
+
+}
+
+///If an ally piece can block the threatening piece 
+fn can_block_threat() {
+
+    //Si on peut bloquer, simuler de nouveau le nouveau board avec is_king_exposed pour checker
+    //plusieurs threats
+}
+
+///If an ally piece can take the threatening piece 
+fn can_capture_threat() {
+
+    //Si on peut bloquer, simuler de nouveau le nouveau board avec is_king_exposed pour checker
+    //plusieurs threats
+}
