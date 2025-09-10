8b49de70318abde675cd8e4c8156d9d7603695a9
muffin <muffin@arch.man>
2025-09-10 22:25:48 +0200

    refacto try_move and chessApp

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -2,3 +2,4 @@ pub mod chessapp_struct;
 pub mod render;
 pub mod try_move;
 pub mod inputs;
+pub mod panels;
--- src/gui/try_move.rs
+++ src/gui/try_move.rs
@@ -6,44 +6,37 @@ use crate::validate_move;
 use crate::mat_or_pat;
 
 impl ChessApp {
-    pub fn from_move_to_pgn(&mut self, move_to_encode: (Coord, Coord)) {
-        let (from, to) = move_to_encode;
-        let piece_char = match self.current.board.get(&from).get_piece() {
-            Some(Pawn)  => None,
-            Some(Rook)  => Some('R'),
-            Some(Knight)=> Some('N'),
-            Some(Bishop)=> Some('B'),
-            Some(Queen) => Some('Q'),
-            Some(King)  => Some('K'),
-            None        => Some('?'),
-        };
 
-        let is_capture = !self.current.board.get(&to).is_empty();
-
-        let dest_col = (b'a' + to.col as u8) as char; 
-        let dest_row = char::from_digit((to.row + 1) as u32, 10).unwrap(); 
-
-        self.current.last_move_pgn = String::new();
-        if let Some(pc) = piece_char {
-            self.current.last_move_pgn.push(pc);
-        } else if is_capture {
-            let src_col = (b'a' + from.col as u8) as char;
-            self.current.last_move_pgn.push(src_col);
+    pub fn try_move(&mut self, from: Coord, to: Coord) {
+        if !self.current.board.is_legal_move(&from, &to, &self.current.active_player) {
+            println!("Illegal move: {from:?} -> {to:?}");
+            return ;
         }
-
-        if is_capture {
-            self.current.last_move_pgn.push('x');
+        if validate_move::is_king_exposed(&from, &to, &self.current.active_player, &self.current.board) {
+            println!("King is exposed: illegal move");
+            return ;
         }
+        self.from_move_to_pgn((from, to));
+        self.add_history();
+        println!("{:?} : {:?}", self.current.active_player, self.current.last_move_pgn);
+        self.undo.push(self.current.clone());
+        self.current.board.update_board(&from, &to, &self.current.active_player);
+        self.update_castles(&to);
+        self.redo.clear();
+        self.current.last_move = Some((from, to));
+        if self.autoflip {
+            self.flip = !self.flip;
+        }
+        self.incremente_turn();
+        self.events_check();
+    }
 
-        self.current.last_move_pgn.push(dest_col);
-        self.current.last_move_pgn.push(dest_row);
-        //ajouter le check
-        //ajouter le mat / pat
-        //ajouter la promotion
-        //ajouter ambiguite
-        //ajouter les roques
-    } 
-
+    fn incremente_turn(&mut self) {
+        if self.current.active_player == Color::Black {
+            self.current.turn += 1;
+        }
+    }
+    
     fn add_history(&mut self) {
         if self.current.active_player == Color::White {
             let turn_str = self.current.turn.to_string();
@@ -103,34 +96,41 @@ impl ChessApp {
         }
     }
 
-    fn incremente_turn(&mut self) {
-        if self.current.active_player == Color::Black {
-            self.current.turn += 1;
-        }
-    }
+    pub fn from_move_to_pgn(&mut self, move_to_encode: (Coord, Coord)) {
+        let (from, to) = move_to_encode;
+        let piece_char = match self.current.board.get(&from).get_piece() {
+            Some(Pawn)  => None,
+            Some(Rook)  => Some('R'),
+            Some(Knight)=> Some('N'),
+            Some(Bishop)=> Some('B'),
+            Some(Queen) => Some('Q'),
+            Some(King)  => Some('K'),
+            None        => Some('?'),
+        };
 
-    pub fn try_move(&mut self, from: Coord, to: Coord) {
-        if !self.current.board.is_legal_move(&from, &to, &self.current.active_player) {
-            println!("Illegal move: {from:?} -> {to:?}");
-            return ;
-        }
-        if validate_move::is_king_exposed(&from, &to, &self.current.active_player, &self.current.board) {
-            println!("King is exposed: illegal move");
-            return ;
-        }
-        self.from_move_to_pgn((from, to));
-        self.add_history();
-        println!("{:?} : {:?}", self.current.active_player, self.current.last_move_pgn);
-        self.undo.push(self.current.clone());
-        self.current.board.update_board(&from, &to, &self.current.active_player);
-        self.update_castles(&to);
-        self.redo.clear();
-        self.current.last_move = Some((from, to));
-        if self.autoflip {
-            self.flip = !self.flip;
-        }
-        self.incremente_turn();
-        self.events_check();
-    }
+        let is_capture = !self.current.board.get(&to).is_empty();
 
+        let dest_col = (b'a' + to.col as u8) as char; 
+        let dest_row = char::from_digit((to.row + 1) as u32, 10).unwrap(); 
+
+        self.current.last_move_pgn = String::new();
+        if let Some(pc) = piece_char {
+            self.current.last_move_pgn.push(pc);
+        } else if is_capture {
+            let src_col = (b'a' + from.col as u8) as char;
+            self.current.last_move_pgn.push(src_col);
+        }
+
+        if is_capture {
+            self.current.last_move_pgn.push('x');
+        }
+
+        self.current.last_move_pgn.push(dest_col);
+        self.current.last_move_pgn.push(dest_row);
+        //ajouter le check
+        //ajouter le mat / pat
+        //ajouter la promotion
+        //ajouter ambiguite
+        //ajouter les roques
+    } 
 }
