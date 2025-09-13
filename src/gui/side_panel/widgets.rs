1b360deabef662ef39ebc46f6ade376ed75916d9
muffin <muffin@arch.man>
2025-09-13 04:04:30 +0200

    wasm success

--- src/gui/side_panel/widgets.rs
+++ src/gui/side_panel/widgets.rs
@@ -59,30 +59,30 @@ impl ChessApp {
         });
     }
 
-    pub fn new_save_load(&mut self, ui: &mut egui::Ui, ctx: &Context) {
-        ui.horizontal(|ui| {
-            if ui.button("New game").clicked() {
-                *self = ChessApp::default();
-            }
-
-            if ui
-                .add_enabled(!(self.undo.is_empty()), egui::Button::new("Save"))
-                .clicked()
-            {
-                self.file_dialog.save_file();
-                ui.label(format!("save file: {:?}", self.file_path));
-            }
-            if let Some(path) = self.file_dialog.update(ctx).picked() {
-                if let Some(path) = Some(path.to_path_buf()) {
-                    println!("{:?}", path);
-                }
-                export_pgn(&self.current.history_san, path);
-            }
-            if ui.add_enabled(false, egui::Button::new("Load")).clicked() {
-                println!("Load game");
-            }
-        });
-    }
+    // pub fn new_save_load(&mut self, ui: &mut egui::Ui, ctx: &Context) {
+    //     ui.horizontal(|ui| {
+    //         if ui.button("New game").clicked() {
+    //             *self = ChessApp::default();
+    //         }
+    //
+    //         if ui
+    //             .add_enabled(!(self.undo.is_empty()), egui::Button::new("Save"))
+    //             .clicked()
+    //         {
+    //             self.file_dialog.save_file();
+    //             ui.label(format!("save file: {:?}", self.file_path));
+    //         }
+    //         if let Some(path) = self.file_dialog.update(ctx).picked() {
+    //             if let Some(path) = Some(path.to_path_buf()) {
+    //                 println!("{:?}", path);
+    //             }
+    //             export_pgn(&self.current.history_san, path);
+    //         }
+    //         if ui.add_enabled(false, egui::Button::new("Load")).clicked() {
+    //             println!("Load game");
+    //         }
+    //     });
+    // }
 
     pub fn side_panel_flip(&mut self, ui: &mut egui::Ui) {
         ui.horizontal(|ui| {
