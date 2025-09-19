78de9047ec005a835f8e132704a673157caa3e09
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2025-09-20 01:58:29 +0200

    better right panel

--- src/gui/desktop_ui/left_panel.rs
+++ src/gui/desktop_ui/left_panel.rs
@@ -1,20 +1,21 @@
 use crate::ChessApp;
-
+use crate::gui::chessapp_struct::AppMode;
+use crate::gui::chessapp_struct::AppMode::*;
 use egui::Context;
 
 impl ChessApp {
     //Shows turn infos, resign / draw options, new game option and timer options
     pub fn left_panel_desktop(&mut self, ctx: &Context) {
         egui::SidePanel::left("left_panel")
-            .default_width(230.0)
+            .default_width(270.0)
             .show(ctx, |ui| {
                 self.turn_infos(ui);
-                if !self.history.is_empty() {
+                if matches!(&self.app_mode, AppMode::Versus(None)) {
                     self.draw_resign_undo(ui);
                 }
                 // ui.separator();
                 self.new_save_load(ui, ctx);
-                if self.history.is_empty() || self.current.end.is_some() {
+                if self.app_mode == Lobby {
                     self.timer_increment(ui, ctx);
                 }
             });
