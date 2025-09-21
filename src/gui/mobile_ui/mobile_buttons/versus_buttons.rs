f576a1476892dacb38d0db1c16e6039327c82ad9
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2025-09-21 15:47:26 +0200

    Undo limit added

--- src/gui/mobile_ui/mobile_buttons/versus_buttons.rs
+++ src/gui/mobile_ui/mobile_buttons/versus_buttons.rs
@@ -1,6 +1,8 @@
 use crate::ChessApp;
+use crate::Color::*;
 use crate::gui::chessapp_struct::End::Draw;
 use crate::gui::game_state_struct::DrawOption;
+use crate::gui::hooks::WinDia;
 use crate::gui::hooks::WinDia::*;
 use crate::gui::mobile_ui::mobile_buttons::versus_buttons::DrawOption::Available;
 
@@ -34,14 +36,33 @@ impl ChessApp {
         } else {
             ui.add_space(150.0);
         }
-        if ui
-            .add_enabled(
-                self.win.is_none() && !self.history.snapshots.is_empty(),
-                egui::Button::new("Undo"),
-            )
-            .clicked()
+        #[allow(clippy::collapsible_if)]
+        if self.settings.allow_undo
+            && (self.settings.white_undo > 0 || self.settings.black_undo > 0)
         {
-            self.win = Some(Undo);
+            #[allow(clippy::collapsible_if)]
+            if ui
+                .add_enabled(
+                    self.current.end.is_none()
+                        && self.can_undo()
+                        && self.win.is_none()
+                        && (self.history.snapshots.len() > 1
+                            || self.history.snapshots.len() == 2
+                                && self.current.active_player == White),
+                    egui::Button::new("Undo"),
+                )
+                .clicked()
+            {
+                self.win = Some(WinDia::Undo);
+                match self.current.opponent {
+                    White => {
+                        self.settings.white_undo -= 1;
+                    }
+                    Black => {
+                        self.settings.black_undo -= 1;
+                    }
+                }
+            }
         }
     }
 }
