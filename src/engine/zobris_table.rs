0bb261649fffa105bb8d28caf665164fb6688efc
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2026-04-27 23:37:25 +0200

    doc: comments for TT, Zobrist hash, and minimax

--- src/engine/zobris_table.rs
+++ src/engine/zobris_table.rs
@@ -11,9 +11,22 @@ pub struct ZobristTable {
     pub en_passant: [u64; 8],
 }
 
+//generate() will return a struct containing 768 random values
+// at call of generate(), we compose the hash of the first board : en empty board, where for each square we XOR "in" all pieces on their starting position
+// This allows us to calculate new hashs very efficiently : instead of recalculating a new hash for each new board, we XOR in and XOR out the differences between them
+// Since each turn is a piece moving and optionaly a piece captured, we can incrementaly deduce the hash of the new board following these steps :
+//  XOR the moving piece from its origin : since it was hashed for our previous board, it's a double XOR, so the new hash match a board where this cell is empty,
+//  XOR the the piece at dest in case of capture (same logic as above)
+//  XOR the moving piece at dest
+//  Castles, en_passant and player on trait must be hashed too
+//Doing so, we have the exact same resulted hash for the new board, but with way fewer operations than generating a hash from scratch
 impl ZobristTable {
     fn generate() -> Self {
-        let mut state: u64 = 0xDEADBEEF_CAFEBABE;
+        //That's our seed
+        let mut state: u64 = 0x9E3779B97F4A7C15;
+
+        //next is a closure, capturing state as &mut, we use it as a manual iterator
+        // using state as seed, we will have a new seeded u64 at each call of next();
         let mut next = || lcg_rand(&mut state);
 
         let mut pieces = [[0; 64]; 12];
@@ -39,12 +52,17 @@ impl ZobristTable {
     }
 }
 
+// our table MUST be init once, and accessible anytime : a const with Onelock to read it as quick as any variable (Mutex would imply a read lock)
 static ZOBRIST: OnceLock<ZobristTable> = OnceLock::new();
 
+//&'static : the table will live for the all program duration
 pub fn zobrist() -> &'static ZobristTable {
     ZOBRIST.get_or_init(|| ZobristTable::generate())
 }
 
+//to generate a pseudo-random sequence, we often use state = state * A + B were A and B are defined constants (chosen to guarantee long period and bit distribution)
+//Theses constants guarantee a good distribution of bits on the all u64 space, thus, there is still a risk of collision, but very low (≈ 1/2^64 for each duo of boards)
+// https://en.wikipedia.org/wiki/Linear_congruential_generator#Parameters_in_common_use
 fn lcg_rand(state: &mut u64) -> u64 {
     *state = state
         .wrapping_mul(6364136223846793005)
@@ -52,6 +70,7 @@ fn lcg_rand(state: &mut u64) -> u64 {
     *state
 }
 
+// on utilise toujours cette fonction a defaut de bien gerer en_passant castle
 pub fn hash_from_scratch(board: &Board, active: Color) -> u64 {
     let zt = zobrist();
     let mut h: u64 = 0;
