d8be94ea71a7f11ed7c7ca219f26950052b0d6e9
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2026-05-03 00:42:49 +0200

    fix: bench.hmtl prod and fix button on mobile

--- src/gui/components/timer_window.rs
+++ src/gui/components/timer_window.rs
@@ -8,6 +8,12 @@ impl ChessApp {
             .anchor(egui::Align2::CENTER_CENTER, [0.0, -365.0])
             .show(ctx, |ui| {
                 self.timer_increment(ui, ctx);
+                ui.separator();
+                ui.vertical_centered(|ui| {
+                    if ui.button("Save").clicked() {
+                        self.win = None;
+                    }
+                });
             });
     }
 }
